9a157c7ca6e876565ab50688837b39cc11dd15ec
xzpjerry <xzpjerry@gmail.com>
2021-08-17 11:36:51 +0800

    ref: updated obfucate func; used pyperclip waitForNewPaste; used gzip

--- py_handoff/main.py
+++ py_handoff/main.py
@@ -1,80 +1,82 @@
+import json
 import socket
-from time import sleep
+import threading
+import uuid
+import gzip
 
 import pyperclip
 
 PORT = 5005
 KEY = "this is a secret key!"
+
+self_id = uuid.uuid4().hex
 previous_clipboard = ""
 
 
-def obfuscate(string, key=None):
-    key = key or KEY
-    encoded_chars = []
-    for i in range(len(string)):
-        key_c = key[i % len(key)]
-        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
-        encoded_chars.append(encoded_c)
-    encoded_string = "".join(encoded_chars)
-    return encoded_string
+def obfuscate(byt):
+    # Use same function in both directions.  Input and output are bytes
+    # objects.
+    mask = KEY.encode()
+    lmask = len(mask)
+    return bytes(c ^ mask[i % lmask] for i, c in enumerate(byt))
 
 
-def clearify(string, key=None):
-    key = key or KEY
-    encoded_chars = []
-    for i in range(len(string)):
-        key_c = key[i % len(key)]
-        encoded_c = chr((ord(string[i]) - ord(key_c) + 256) % 256)
-        encoded_chars.append(encoded_c)
-    encoded_string = "".join(encoded_chars)
-    return encoded_string
+def encode_msg(msg):
+    msg = json.dumps({"msg": msg, "from": self_id,})
+    msg = msg.encode()
+    msg = gzip.compress(msg, compresslevel=9)
+    msg = obfuscate(msg)
+    return msg
 
 
-def blocking_get_clipboard_if_changed():
-    global previous_clipboard
-    while True:
-        tmp = pyperclip.paste()
-        if tmp != previous_clipboard:
-            previous_clipboard = tmp
-            return previous_clipboard
-        sleep(1)
+def decode_msg(data):
+    data = obfuscate(data)
+    data = gzip.decompress(data)
+    data = data.decode()
+    data = json.loads(data)
+    return data["msg"], data["from"]
 
 
-def paste():
+def server():
     """Broadcasting clipboard changes to LAN
     """
     while True:
-        msg = blocking_get_clipboard_if_changed()
+        msg = pyperclip.waitForNewPaste()
         interfaces = socket.getaddrinfo(
             host=socket.gethostname(), port=None, family=socket.AF_INET
         )
-        msg = obfuscate(msg).encode()
+        msg = encode_msg(msg)
         for ip in {ip[-1][0] for ip in interfaces}:
-            sock = socket.socket(
-                socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
-            )  # UDP
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            sock.bind((ip, 0))
+            try:
+                sock = socket.socket(
+                    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP
+                )  # UDP
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                sock.bind((ip, 0))
 
-            sock.sendto(msg, ("255.255.255.255", PORT))
-            sock.close()
+                sock.sendto(msg, ("255.255.255.255", PORT))
+                sock.close()
+            except Exception as e:
+                print(e)
 
 
-def copy():
+def client():
     """Subscribing clipboard changes from remote, and updating to local
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("0.0.0.0", PORT))
     while True:
-        data = sock.recv(1024).decode()
-        pyperclip.copy(clearify(data))
+        data = sock.recv(1024)
+        msg, from_id = decode_msg(data)
+        if from_id != self_id:
+            try:
+                pyperclip.copy(msg)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == "__main__":
-    import threading
-
-    server_thread = threading.Thread(target=paste)
+    server_thread = threading.Thread(target=server)
     server_thread.start()
-    copy()
-
+    client()
