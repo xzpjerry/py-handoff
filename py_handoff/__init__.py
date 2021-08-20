c8eddb4d79db65bb5f4ce65d65ab391abb445b0b
xzpjerry <xzpjerry@gmail.com>
2021-08-20 14:06:58 +0800

    ref: updated plugin spec

--- py_handoff/__init__.py
+++ py_handoff/__init__.py
@@ -1,4 +1,5 @@
 import pluggy
+
 from py_handoff.interface import BaseMessage
 
 hookspec = pluggy.HookspecMarker("py_handoff")
@@ -7,8 +8,12 @@ hookimpl = pluggy.HookimplMarker("py_handoff")
 
 class PyHandoffPluginSpec:
     @hookspec
+    def waiting_to_capture(self):
+        """This method should be blocking until it want to capture the interested state"""
+
+    @hookspec
     def capture_state(self) -> BaseMessage:
-        """Define how to capture the state that you want to share with the remote
+        """Define how to capture the interested state that you want to share with the remote
 
         Returns:
             BaseMessage: Captured state
@@ -16,8 +21,8 @@ class PyHandoffPluginSpec:
 
     @hookspec
     def restore_state(self, msg: BaseMessage):
-        """Define how to restore the state at the remote side
+        """Define how to restore the interested state at the remote side
 
         Args:
-            msg (BaseMessage): [description]
+            msg (BaseMessage): Restore the state based on the message
         """
